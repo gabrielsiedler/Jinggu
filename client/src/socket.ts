d12d685459cf5ba272b88c90fe86fb4e461d4930
Gabriel Siedler <gabrielsiedler@gmail.com>
2022-05-06 16:58:03 -0300

    adds dotenv

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv'
 import { io } from 'socket.io-client'
 
 import { Core } from './Core'
 import { Direction } from './player/player.i'
 
-const socket = io('https://4b0c-2804-1b3-8401-f304-5d8-fc1f-81e-5f3.sa.ngrok.io')
+dotenv.config()
+
+const socket = io(process.env.SERVER_URL!)
 
 export let core: Core
 
